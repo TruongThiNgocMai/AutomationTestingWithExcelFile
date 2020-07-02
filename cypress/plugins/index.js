94ad7797d2a6f7c65df602e153ae3abdbe9da993
unknown <admin@PNVD060.pn.com.vn>
2020-07-02 18:20:45 +0700

    New exercise related to working with excel file using Cypress - create a new file - execute script - save data into excel file

---
+++ cypress/plugins/index.js
@@ -0,0 +1,37 @@
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
+
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
+
+/**
+ * @type {Cypress.PluginConfig}
+ */
+
+// const xlsx = require("node-xlsx").default;
+// const fs = require("fs");
+// const path = require("path");
+
+module.exports = (on, config) => {
+  // `on` is used to hook into various events Cypress emits
+  // on("task", {
+  //   parseXlsx({ filePath }) {
+  //     return new Promise((resolve, reject) => {
+  //       try {
+  //         const jsonData = xlsx.parse(fs.readFileSync(filePath));
+  //         resolve(jsonData);
+  //       } catch (e) {
+  //         reject(e);
+  //       }
+  //     });
+  //   }
+  // });
+};
