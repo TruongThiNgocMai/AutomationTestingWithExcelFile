fd8dc5b87273ee326fa343ea4053d3a6216a9256
unknown <admin@PNVD060.pn.com.vn>
2020-07-03 19:03:12 +0700

    excel update

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,22 +16,8 @@
  * @type {Cypress.PluginConfig}
  */
 
-// const xlsx = require("node-xlsx").default;
-// const fs = require("fs");
-// const path = require("path");
+const cucumber = require('cypress-cucumber-preprocessor').default
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // on("task", {
-  //   parseXlsx({ filePath }) {
-  //     return new Promise((resolve, reject) => {
-  //       try {
-  //         const jsonData = xlsx.parse(fs.readFileSync(filePath));
-  //         resolve(jsonData);
-  //       } catch (e) {
-  //         reject(e);
-  //       }
-  //     });
-  //   }
-  // });
-};
+  on('file:preprocessor', cucumber())
+}
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,44 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+const urlDanatrain = 'https://dana-train-web-admin-stg.enouvo.com/'
+
+//--------------Function For Login Successfully to Dana Train Website---------------
+
+Cypress.Commands.add('LoginDanaFunction', (username, password) => {
+    cy.visit(urlDanatrain);
+    cy.get('#username').click().type(username);
+    cy.get('#password').click().type(password);
+    cy.get('.ant-btn').click();
+    cy.wait(3000);
+})
+
+//--------------Function For Upload A New Image/File---------------
+
+Cypress.Commands.add('newUploadBlobFile', (fileName, fileType) => {
+    cy.get("input[type='file']").then($input => {
+        cy.fixture(fileName, 'base64')
+            .then(Cypress.Blob.base64StringToBlob)
+            .then(blob => {
+                const el = $input[0];
+                const testFile = new File([blob], fileName, { type: fileType });
+                const dataTransfer = new DataTransfer();
+                dataTransfer.items.add(testFile);
+                el.files = dataTransfer.files;
+                return cy.wrap($input).trigger('change', { force: true });
+            });
+    });
+    cy.wait(5000)
+})
+
+//Cause after create a new dispatch, manager account login recently so do not visit to url
+Cypress.Commands.add('LoginDanaFunctionToCheckDispatch', (username, password) => {
+    cy.get('#username').click().type(username);
+    cy.get('#password').click().type(password);
+    cy.get('.ant-btn').click();
+    cy.wait(3000);
+})
+
+Cypress.Commands.add('LogoutDanaFunctionForWebMac', () => {
+    //Cause have cy.reload() function, so we need handle click on X icon on the form to logout
+    cy.get('.h4 > .anticon').click()
+    cy.get('.ant-avatar').click()
+    cy.get('.ant-dropdown-menu > :nth-child(3)').click();
+})
